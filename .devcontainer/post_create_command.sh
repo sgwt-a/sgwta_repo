eda7fe3a571f219f4f6f719e04445606a4644e8b
sgwt-a <wataruzemi@gmali.com>
2024-09-14 08:47:15 +0000

    update pip install and post_create_command

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-# pre-commit
-pre-commit install
-
-# python
-pip3 install -r requirements.txt
+pwd
 
 # git config
-./setting_git_config.sh
+.devcontainer/setting_git_config.sh
+
+# python: requirements.txtで指定したバージョンがインストールできないケースが多発したためコメントアウト。必要最低限のもののみDockerfileでインストールすることにした。
+# pip3 install -r .devcontainer/requirements.txt
+
+# pre-commit
+## 上記のgit config設定をしないとpre-commitがエラーになる
+pre-commit install
